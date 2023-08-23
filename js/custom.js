cfffb509146d78653a988926c98b99e6cdaeb8e5
Zach Nash <afreakenturtle@gmail.com>
2023-08-23 10:22:50 -0400

    removed map from the website

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,3 @@
-// to get current year
-function getYear() {
-    var currentDate = new Date();
-    var currentYear = currentDate.getFullYear();
-    document.querySelector("#displayYear").innerHTML = currentYear;
-}
-
-getYear();
-
-
-
 // owl carousel slider js
 var owl = $('.portfolio_carousel').owlCarousel({
     loop: true,
@@ -50,14 +39,6 @@ $('.owl-filter-bar').on('click', '.item', function (e) {
 
     e.preventDefault();
 })
-/** google_map js **/
-function myMap() {
-    var mapProp = {
-        center: new google.maps.LatLng(40.712775, -74.005973),
-        zoom: 18,
-    };
-    var map = new google.maps.Map(document.getElementById("googleMap"), mapProp);
-}
 
 // nice select
 $(document).ready(function () {
